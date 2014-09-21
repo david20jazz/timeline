3ff72c05e9cb72401d61d1a6ec8b968365dcabd9
ubuntu-jazz <david20jazz@gmail.com>
2014-09-21 15:47:18 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timeline::Application.config.secret_key_base = '548a8a83ceb4a797ddaed5e361bb4ce1034267b1b7e4560164007949bcb833179ba7f4d24d3d1ad168b9dbd9e9be4b621f006ba73cc2747db7df4f55a862a085'
